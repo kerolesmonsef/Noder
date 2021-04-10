34c9407eeb38e68e6b2cabd8f3dcfa2406dc91f3
= <keroelsmonsef@gmail.com>
2021-04-10 14:23:02 +0200

    df

--- test2.js
+++ test2.js
@@ -1,15 +1,33 @@
 import express from 'express';
-const app = express()
+const app = express();
 
-app.get('/:browser', (req, res) => {
+
+const myPromise = (browser) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`start ${browser}`);
+            for (let i = 0; i < 199999999; i++) {
+                const x = i * 654 * Math.random();
+            }
+            console.log(`end ${browser}`);
+            resolve(browser);
+        }, 0);
+    });
+}
+
+
+app.get('/:browser',  (req, res) => {
     const browser = req.params.browser;
+    setTimeout(() => {
+        console.log(`start ${browser}`);
+        for (let i = 0; i < 299999999; i++) {
+            const x = i * 654 * Math.random();
+        }
+        console.log(`end ${browser}`);
+    }, 0);
+    res.send(`end ${browser}`)
+});
 
-    console.log(`begin ${browser}`);
-    for (let i = 0; i < 99999999; i++) {
-        const x = i * 654 * Math.random();
-    }
-    console.log(`end ${browser}`);
-    res.end(`end ${browser}`)
-})
 
-app.listen(3000)
+
+app.listen(4000);
