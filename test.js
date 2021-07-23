0342ad4f832c1233624d2f37e1cb3b091970586c
keroles-star <keroles@ue-systems.com>
2021-07-23 23:56:25 +0200

    fd

--- test.js
+++ test.js
@@ -1,17 +1,24 @@
 const express = require('express');
 
 const app = express();
+const app2 = express();
 
-let name = "No Name";
 
-app.get("/",(req,res)=>{
-    if(req.query.name){
-        name = req.query.name;
+app.get("/", (req, res) => {
+    for (let i = 0; i < 99999999999; i++) {
+
     }
+    res.send("end");
+});
 
-    res.send(name);
+app2.get("/test", (req, res) => {
+    res.send("end app2");
 });
 
 app.listen(4000, () => {
-    console.log(`Server started on port`);
+    console.log(`Server started on port 4000`);
+});
+
+app2.listen(5000, () => {
+    console.log(`Server started on port 5000`);
 });
